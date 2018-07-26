bd1c5261e403eecf6dad90cc43ba13dd43e823b3
Alexander <adam.alexander@sos.alabama.gov>
2018-07-26 15:41:05 -0500

    Add Display Code to Test getters and setters for player attributes

--- PlayerTestDrive.java
+++ PlayerTestDrive.java
@@ -5,9 +5,17 @@ class PlayerTestDrive{
 		p1.setName("Embalyst");
 		p1.setStrength((byte)16);
 		p1.setIntelligence((byte)14);
+		p1.setWisdom((byte)15);
+		p1.setDexterity((byte)12);
+		p1.setConstitution((byte)13);
+		p1.setCharisma((byte)9);
 
 		System.out.println("Player's name is: " + p1.getName());
 		System.out.println("Strength: " + p1.getStrength());
 		System.out.println("Intelligence: " + p1.getIntelligence());
+		System.out.println("Widsom: " + p1.getWidsom());
+		System.out.println("Dexterity: " + p1.getDexterity());
+		System.out.println("Constitution: " + p1.getConstitution());
+		System.out.println("Charisma: " + p1.getCharisma());
 	}
 }
